8c0dc56a0b73dc19636611f6f70a3669a848d66b
zkpc <14496505@qq.com>
2019-10-15 11:01:11 +0800

    基本可以跑起来了

--- ssm_bps/src/test/java/ssm/RedisTest.java
+++ ssm_bps/src/test/java/ssm/RedisTest.java
@@ -9,16 +9,21 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(locations = { "classpath:spring-application.xml" })
+@ContextConfiguration(locations = {"classpath:spring-application.xml"})
 public class RedisTest {
-	
+
 	private static final Logger LOGGER = Logger.getLogger(RedisTest.class);
 	@Autowired
 	private RedisUtil redisUtil;
 
 	@Test
 	public void redisTest() {
-		this.redisUtil.set("hello", "world");
+		this.redisUtil.set("hello1", "world1");
+		this.redisUtil.set("hello2", "world2");
+		this.redisUtil.set("hello3", "world3");
+		this.redisUtil.set("hello4", "world4");
+		this.redisUtil.set("hello5", "world5");
 		System.out.println(this.redisUtil.get("hello"));
+//		System.out.println(this.redisUtil.get("zk"));
 	}
 }
--- ssm_bps/src/test/java/ssm/test.java
+++ ssm_bps/src/test/java/ssm/test.java
@@ -1,5 +1,8 @@
 package ssm;
 
+import com.alibaba.fastjson.JSON;
+import com.ssm.pojo.User;
+import com.ssm.service.UserService;
 import org.apache.log4j.Logger;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,19 +10,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-import com.alibaba.fastjson.JSON;
-import com.ssm.pojo.User;
-import com.ssm.service.UserService;
-
-@RunWith(SpringJUnit4ClassRunner.class)     //表示继承了SpringJUnit4ClassRunner类  
-@ContextConfiguration(locations = {"classpath:spring-mybatis.xml"}) 
+@RunWith(SpringJUnit4ClassRunner.class)     //表示继承了SpringJUnit4ClassRunner类
+@ContextConfiguration(locations = {"classpath:spring-application.xml"})
 public class test {
 
 	private final static Logger LOGGER = Logger.getLogger(test.class);
-	
+
 	@Autowired
 	private UserService userService;
-	
+
 	@Test
 	public void testOne(){
 		User user = userService.findUser();
